676a8f6bf5126b00c52d1cef1b4dff9cd19b867c
Ankitaofficialgithub <ankitamajumder400@gmail.com>
2024-10-19 00:52:31 +0530

    First v1

---
+++ src/main/java/com/api/A.java
@@ -0,0 +1,44 @@
+package com.api;
+import java.util.*;
+public class A {
+//    public static void main(String[] args){
+//        A a1=new A();
+//        int val =a1.test();
+//        System.out.println(val);
+//        C c1= new C();
+//        c1.test2();
+//    }
+//    public int test(){
+//       System.out.println("Hello");
+//       return 100;
+//        B b1= new B();
+//        return b1.test1();
+//   }
+//public static boolean isPalindrome(String str) {
+//    int left = 0;
+//    int right = str.length() - 1;
+//
+//    while (left < right) {
+//        if (str.charAt(left) != str.charAt(right)) {
+//            return false;
+//        }
+//        left++;
+//        right--;
+//    }
+//    return true;
+//}
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a string: ");
+//        String input = scanner.nextLine();
+
+//        if (isPalindrome(input)) {
+//            System.out.println(input + " is a palindrome.");
+//        } else {
+//            System.out.println(input + " is not a palindrome.");
+//        }
+//
+//        scanner.close();
+//    }
+}
